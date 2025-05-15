d7b7adea5bc15f886e90bcdbeded2b1ec28b256f
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-05-15 15:09:33 -0300

    Finalizado

--- receiver.v
+++ receiver.v
@@ -1,12 +1,44 @@
 module receiver (
     input clk,
     input rstn,
-    output ready,
-    output [6:0] data_out,
-    output parity_ok_n,
+    output reg ready,
+    output reg [6:0] data_out,
+    output reg parity_ok_n,
     input serial_in
 );
+    reg [3:0] contador_bits;
+    reg [7:0] armazenador_bits;
+    reg recebendo_dados;
 
-//insira seu código aqui
+    always @(posedge clk or negedge rstn) begin
+        if (!rstn) begin
+            contador_bits <= 0;
+            armazenador_bits <= 0;
+            recebendo_dados <= 0;
+            data_out <= 0;
+            ready <= 0;
+            parity_ok_n <= 1;
+        end else begin
+            if (!recebendo_dados) begin
+                if (serial_in == 0) begin 
+                    recebendo_dados <= 1;
+                    contador_bits <= 0;
+                end
+            end else if (recebendo_dados) begin
+                if (contador_bits < 8) begin
+                    armazenador_bits <= {serial_in, armazenador_bits[7:1]};
+                end
+                
+                contador_bits <= contador_bits + 1;
+
+                if (contador_bits == 8) begin
+                    data_out <= armazenador_bits[6:0];
+                    parity_ok_n <= (^armazenador_bits == 1'b0) ? 1:0; 
+                    ready <= 1;
+                    recebendo_dados <= 0;
+                end
+            end
+        end
+    end
 
 endmodule
--- transmitter.v
+++ transmitter.v
@@ -5,7 +5,37 @@ module transmitter (
     input [6:0] data_in,
     output reg serial_out
 );
+    reg [3:0] contador_bits;
+    reg [7:0] armazenador_bits;
+    reg enviando_dados;
 
-//insira seu código aqui
+    wire bit_paridade = ~ (^data_in); 
+
+    always @(posedge clk or negedge rstn) begin
+        if (!rstn) begin
+            serial_out <= 1'b1; 
+            armazenador_bits <= 8'd0;
+            contador_bits <= 0;
+            enviando_dados <= 0;
+        end else begin
+            if (start && !enviando_dados) begin    
+                armazenador_bits <= {bit_paridade, data_in};
+                contador_bits <= 0;
+                serial_out <= 1'b0; 
+                enviando_dados <= 1;
+            end else if (enviando_dados) begin
+                
+                contador_bits <= contador_bits + 1;
+
+                if (contador_bits < 8) begin
+                    serial_out <= armazenador_bits[0];
+                    armazenador_bits <= {1'b0, armazenador_bits[7:1]};
+                end else begin
+                    serial_out <= 1'b1; 
+                    enviando_dados <= 0;
+                end
+            end
+        end
+    end
 
 endmodule
